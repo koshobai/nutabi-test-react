1a0dd57ace0698c5119aeb5207d53316f7a1ac29
Tariq Lacy <hello@koshobai.com>
2025-09-12 10:22:39 +0900

    update comments to affirm understanding of the interplay between the two servers

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -6,7 +6,12 @@ import "./Home.css"
 import EntryList from "../../components/EntryList"
 
 export default function Home() {
-  const { data, isPending, error } = useFetch("http://localhost:8000/entries?")
+  //I'm fetching data from a local JSON server running on port 3000
+  //after this, I still need to run "npm start" in a separate terminal window
+  //it'll ask if I want to run on another port; I say "Y" and it runs on 3001
+  //from there, I can access the raw json data at http://localhost:3000/entries
+  //and the react app at http://localhost:3001
+  const { data, isPending, error } = useFetch("http://localhost:3000/entries")
 
   return (
     <div className="home">
