444c2a2dcec3b25b146d511bfec025df21638a84
Tariq Lacy <hello@koshobai.com>
2025-09-12 12:12:36 +0900

    update the directory of the db.json file so that people on GitHub and those viewing the web app can access it

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -6,18 +6,13 @@ import "./Home.css"
 import EntryList from "../../components/EntryList"
 
 export default function Home() {
-  //I'm fetching data from a local JSON server running on port 3000
-  //after this, I still need to run "npm start" in a separate terminal window
-  //it'll ask if I want to run on another port; I say "Y" and it runs on 3001
-  //from there, I can access the raw json data at http://localhost:3000/entries
-  //and the react app at http://localhost:3001
-  const { data, isPending, error } = useFetch("http://localhost:3000/entries")
+  const { data, isPending, error } = useFetch("/db.json")
 
   return (
     <div className="home">
       {error && <p className="error">{error}</p>}
       {isPending && <p className="loading">Loading...</p>}
-      {data && <EntryList entries={data} />}
+      {data && <EntryList entries={data.entries} />}
     </div>
   )
 }
