493d6b0f38c891ae1880fd0e30d71e8e4e6ad50e
Tariq Lacy <hello@koshobai.com>
2025-09-18 17:38:28 +0900

    adjust each navbar to enhance appearance in larger viewports

--- src/components/Futon.js
+++ src/components/Futon.js
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom"
 import "./Futon.css"
 
 export default function Futon() {
--- src/components/Makura.js
+++ src/components/Makura.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom"
+// import { Link } from "react-router-dom"
 import "./Makura.css"
 
 export default function Makura() {
