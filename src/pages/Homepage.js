810cf5d01883ca7a152abf2c58eea4d200c9ce6f
Tariq Lacy <hello@koshobai.com>
2025-11-07 17:09:57 +0900

    update home and homepage

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="page-placeholder">
       <h2>Coming Soon 建設中</h2>
-      <p>Please look forward to a new page here in October!</p>
+      <p>Please look forward to a new page here in November!</p>
     </div>
   )
 }
